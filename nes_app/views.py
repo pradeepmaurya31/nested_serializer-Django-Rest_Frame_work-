29ed369ada4980a2f306803da2cfdae8b28af49b
pradeepmaurya <pm9231@gmail.com>
2022-03-25 13:56:52 +0530

    permission added

--- nes_app/views.py
+++ nes_app/views.py
@@ -1,6 +1,22 @@
+from urllib import request
 from .models import Instructor, Course
 from .serializer import InstructorSerializer, CoursesSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.authentication import BasicAuthentication
+
+
+class WriteUserPermission(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        
+        if request.method == 'GET':
+            return True
+        
+        if request.method == 'POST' or request.method == 'PUT' or request.method == 'DELETE':
+            if user.is_superuser:
+                return True
+        return False
 
 
 class InstructorViewSet(generics.ListCreateAPIView):
@@ -13,6 +29,8 @@ class InstructordetailsView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class CoursesViewSet(generics.ListCreateAPIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated,WriteUserPermission]
     queryset = Course.objects.all()
     serializer_class = CoursesSerializer
     
